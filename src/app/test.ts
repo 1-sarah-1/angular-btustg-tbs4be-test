26956f8b0d32164eb94a028bdc31c53da207aeca
1-sarah-1 <sarah.pfaeffli@sbb.ch>
2022-06-08 11:37:53 +0200

    1 file modified, 2 added, 2 deleted

---
+++ src/app/test.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+/**
+ * @title Select Option Groups Multi Selection
+ * @order 60
+ */
+@Component({
+  selector: 'test',
+  templateUrl: 'test.html',
+})
+export class Test {
+  
+}
